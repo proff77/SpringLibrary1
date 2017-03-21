fba2217d2fb5a03db778c445bdd2726b62499c88
proff77 <azat_galiev@mail.ru>
2017-03-21 16:54:13 +0300

    21.03 16:55

--- src/ru/javabegin/training/springlibrary/objects/Author.java
+++ src/ru/javabegin/training/springlibrary/objects/Author.java
@@ -1,4 +1,35 @@
 package ru.javabegin.training.springlibrary.objects;
 
-public class Author {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Author implements Serializable {
+
+    private String fio;
+    private Date birthday;
+
+    public Author() {
+
+    }
+
+    public Author(String fio, Date birthday) {
+        this.fio = fio;
+        this.birthday = birthday;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 }
--- src/ru/javabegin/training/springlibrary/objects/Book.java
+++ src/ru/javabegin/training/springlibrary/objects/Book.java
@@ -1,4 +1,131 @@
 package ru.javabegin.training.springlibrary.objects;
 
 public class Book {
+
+    private Author author;
+    private Genre genre;
+    private Publisher publisher;
+    private String name;
+    private byte[] content;
+    private Integer pageCount;
+    private String isbn;
+    private Integer publishYear;
+    private byte[] image;
+    private String descr;
+    private Integer rating;
+    private Long voteCount;
+
+    public Book() {
+    }
+
+    public Book(Author author, Genre genre, Publisher publisher, String name, byte[] content, Integer pageCount, String isbn, Integer publishYear, byte[] image, String descr, Integer rating, Long voteCount) {
+        this.author = author;
+        this.genre = genre;
+        this.publisher = publisher;
+        this.name = name;
+        this.content = content;
+        this.pageCount = pageCount;
+        this.isbn = isbn;
+        this.publishYear = publishYear;
+        this.image = image;
+        this.descr = descr;
+        this.rating = rating;
+        this.voteCount = voteCount;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public Integer getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(Integer pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(Integer publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public Long getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Long voteCount) {
+        this.voteCount = voteCount;
+    }
 }
